3fe66de410af57da5e1c5dc0f2b8dfde28561b06
unknown <lol78955@gmail.com>
2025-12-12 15:47:46 +0700

    fix 500-6

--- Config.php
+++ Config.php
@@ -8,4 +8,3 @@ define('DB_PORT', 3306);                      // tuỳ host (nhiều nơi dùng 
 define('DB_NAME', 'if0_40653751_docshare');  // ví dụ tên DB
 define('DB_USER', 'if0_40653751');            // user từ control panel
 define('DB_PASS', 'zYd0SFn81v');        // mật khẩu DB
-?>
