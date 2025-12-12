e1b62fe70f3073b4dd1af55ebf55d44ecd0bd94a
unknown <lol78955@gmail.com>
2025-12-12 15:21:55 +0700

    fix 500-4

--- Config.php
+++ Config.php
@@ -1,5 +1,7 @@
 <?php
-define("SERVERNAME", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-define("DBNAME", "docshare");
+define('DB_HOST', 'sql310.infinityfree.com'); // hoặc '127.0.0.1' nếu local, hoặc ftphost do control panel cung cấp
+define('DB_PORT', 3306);                      // tuỳ host (nhiều nơi dùng 3306)
+define('DB_NAME', 'if0_40653751_docshare');  // ví dụ tên DB
+define('DB_USER', 'if0_40653751');            // user từ control panel
+define('DB_PASS', 'zYd0SFn81v');        // mật khẩu DB
+?>
